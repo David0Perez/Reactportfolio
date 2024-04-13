79c2abeffc41e11c3cee78642122f94d3199f3c1
David0Perez <147769786+David0Perez@users.noreply.github.com>
2024-04-12 21:22:12 -0400

    sidenav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -13,7 +13,8 @@ const Sidenav = () => {
             {
                 nav ? (
                     <div className='fixed w-full h-sceen bg-white/99 flex flex-col justify-center items-cemter z-20'>
-                        <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg ng-gray-100 shadow-gra'>
+                        <a href='#main' 
+                        className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-700 shadow-gray-400 m-2 p-4 cursor-pointer hover: scale-110 ease-in duration-200'>
                             <AiOutLineHome size={20} />
                             <span className='pl-4'>Home</span>
                         </a>
